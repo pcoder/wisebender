1a625279bbb03f7bb506cc9bf237fd80b0289689
pcoder <purple.coder@yahoo.co.uk>
2013-06-16 12:59:40 +0200

    Create a clone of the default Wiselib source when the user creates an account

--- Symfony/src/Ace/UserBundle/Controller/RegistrationController.php
+++ Symfony/src/Ace/UserBundle/Controller/RegistrationController.php
@@ -63,73 +63,10 @@ class RegistrationController extends BaseController
 		$this->container->get('fos_user.user_manager')->updateUser($user);
 		$this->authenticateUser($user);
 
-		/******** MY CHANGES *********/
-		$first_code =
-			"/*
-				Blink
-				Turns on an LED on for one second, then off for one second, repeatedly.
-
-				This example code is in the public domain.
-			*/
-
-			void setup()
-			{
-				// initialize the digital pin as an output.
-				// Pin 13 has an LED connected on most Arduino boards:
-				pinMode(13, OUTPUT);
-			}
-
-			void loop()
-			{
-				digitalWrite(13, HIGH); // set the LED on
-				delay(1000); // wait for a second
-				digitalWrite(13, LOW); // set the LED off
-				delay(1000); // wait for a second
-			}
-			";
-
-		$second_code =
-			"/*
-				Prints an incremental number the serial monitor
-			*/
-			int number = 0;
-
-			void setup()
-			{
-				Serial.begin(9600);
-			}
-
-			void loop()
-			{
-				Serial.println(number++);
-				delay(500);
-			}
-			";
 		//create new projects
 		$username = $user->getUsernameCanonical();
 		$user = json_decode($this->container->get('ace_user.usercontroller')->getUserAction($username)->getContent(), true);
-		$this->container->get('ace_project.sketchmanager')->createprojectAction($user["id"], "Blink Example", $first_code)->getContent();
-		$this->container->get('ace_project.sketchmanager')->createprojectAction($user["id"], "Serial Comm Example", $second_code)->getContent();
-
-		if ($user["referrer_username"])
-			$referrer = json_decode($this->container->get('ace_user.usercontroller')->getUserAction($user["referrer_username"])->getContent(), true);
-
-		if ($user["referral_code"])
-			$referral_code = json_decode($this->container->get('ace_utilities.referralcodecontroller')->useCodeAction($user["referral_code"])->getContent(), true);
-
-		if ($user["referrer_username"] && $referrer["success"])
-		{
-			$this->container->get('ace_user.usercontroller')->setReferrerAction($username, $user["referrer_username"])->getContent();
-			$this->container->get('ace_user.usercontroller')->setKarmaAction($username, 50)->getContent();
-			$this->container->get('ace_user.usercontroller')->setPointsAction($username, 50)->getContent();
-		}
-		else if ($user["referral_code"] && $referral_code["success"])
-		{
-			$this->container->get('ace_user.usercontroller')->setKarmaAction($username, 50)->getContent();
-			$this->container->get('ace_user.usercontroller')->setPointsAction($username, $referral_code["points"])->getContent();
-		}
-		/******** END OF CHANGES *********/
-
+		$this->container->get('ace_project.sketchmanager')->cloneWiselibAction($user["id"])->getContent();
 		return new RedirectResponse($this->container->get('router')->generate('fos_user_registration_confirmed'));
 	}
 
